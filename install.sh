ec1eb387565c253dcdf2aea290b27be4eb4f393f
MIDO <155222995+Midohajhouj@users.noreply.github.com>
2025-03-05 00:04:05 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -40,7 +40,7 @@ install_dependencies() {
 
     # Install Python dependencies
     echo -e "${YELLOW}[*] Installing Python dependencies...${NC}"
-    pip install keyring --break-system-packages >/dev/null 2>&1
+    pip install keyring tqdm --break-system-packages >/dev/null 2>&1
     echo -e "${GREEN}[+] Dependencies installed successfully!${NC}"
 }
 
