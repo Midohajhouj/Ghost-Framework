b224b413d9d4d1647efe51e429ff5497522b07b3
MIDO <155222995+Midohajhouj@users.noreply.github.com>
2025-03-06 06:58:52 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -69,7 +69,7 @@ make_executable() {
 create_symlink() {
     echo -e "${YELLOW}[*] Creating symlink for easy access...${NC}"
     sudo ln -sf "$(pwd)/ghost.py" /usr/local/bin/ghost
-    sudo mv "$(pwd)/ghost.py" /opt
+    sudo mv -f "$(pwd)/ghost.py" /opt
     echo -e "${GREEN}[+] Symlink created! You can now run 'ghost' from anywhere.${NC}"
 }
 
